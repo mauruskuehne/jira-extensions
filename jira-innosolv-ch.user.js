31b279b3474ea04ec2c7d2a8585bf308930ed0ec
Maurus Kühne <mauruskuehne@icloud.com>
2017-12-20 21:12:21 +0100

    stop timer when summaries have been edited

--- jira-innosolv-ch.user.js
+++ jira-innosolv-ch.user.js
@@ -3,6 +3,7 @@
 // @version     1.3.1
 // @namespace   https://github.com/mauruskuehne/jira-extensions/
 // @updateURL   https://github.com/mauruskuehne/jira-extensions/raw/master/jira-innosolv-ch.user.js
+// @download   https://github.com/mauruskuehne/jira-extensions/raw/master/jira-innosolv-ch.user.js
 // @icon        https://dl.dropboxusercontent.com/u/57161259/icons/cs-ohnoes-icon.png
 // @author      Daniel Dähler, Maurus Kühne
 // @include     https://jira.innosolv.ch/*
@@ -31,12 +32,16 @@
             }
         }
 
+        if(summaries.length > 0) {
+            clearInterval(timer);
+        }
+
         return;
     }
 
     // I don't know of a better way of dealing with the ajax than to check every second until
     // we find the elements we want.
-    GM_log("Oh Yes: Timer starting.");
+    GM_log("Timer starting.");
     timer = setInterval(expandSummaries, 1000);
 
 })();
