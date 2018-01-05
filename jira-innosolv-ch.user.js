7b424753a3df9ab26450617fcee4f9dbd4796f0c
Maurus Kühne <mauruskuehne@icloud.com>
2018-01-05 11:50:53 +0100

    fixed table header position

--- jira-innosolv-ch.user.js
+++ jira-innosolv-ch.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        JIRA Extensions
-// @version     1.3.2
+// @version     1.3.3
 // @namespace   https://github.com/mauruskuehne/jira-extensions/
 // @updateURL   https://github.com/mauruskuehne/jira-extensions/raw/master/jira-innosolv-ch.user.js
-// @download   https://github.com/mauruskuehne/jira-extensions/raw/master/jira-innosolv-ch.user.js
+// @download    https://github.com/mauruskuehne/jira-extensions/raw/master/jira-innosolv-ch.user.js
 // @icon        https://dl.dropboxusercontent.com/u/57161259/icons/cs-ohnoes-icon.png
 // @author      Daniel Dähler, Maurus Kühne
 // @include     https://jira.innosolv.ch/*
@@ -15,6 +15,13 @@
     'use strict';
 
     var timer;
+
+    function fixTableSize() {
+        var source = document.querySelector("#tempo-table > div > #issuetable > thead > tr:nth-child(2) > th.left.colHeaderLink.headerrow-summary.padding");
+        var destination = document.querySelector("#stalker > div > div.content-container.tt-content-container > div > div > #issuetable > thead > tr:nth-child(2) > th.left.colHeaderLink.headerrow-summary.padding");
+
+        destination.width = source.offsetWidth - 8;
+    }
 
     function expandSummaries() {
 
@@ -33,6 +40,7 @@
         }
 
         if(summaries.length > 0) {
+            fixTableSize();
             clearInterval(timer);
         }
 
@@ -42,6 +50,7 @@
     // I don't know of a better way of dealing with the ajax than to check every second until
     // we find the elements we want.
     GM_log("Timer starting.");
-    timer = setInterval(expandSummaries, 1000);
+    timer = setInterval(expandSummaries, 10000);
+    window.onresize = fixTableSize;
 
 })();
