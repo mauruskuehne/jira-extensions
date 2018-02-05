ea173e52291f9616a73f06745a0df0145b38286e
quarkonaut <daniel.daehler@gmail.com>
2018-02-05 21:36:53 +0100

    Version bump

--- jira-innosolv-ch.user.js
+++ jira-innosolv-ch.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        JIRA Extensions
-// @version     1.4.3
+// @version     1.4.4
 // @namespace   https://github.com/mauruskuehne/jira-extensions/
 // @updateURL   https://github.com/mauruskuehne/jira-extensions/raw/master/jira-innosolv-ch.user.js
 // @download    https://github.com/mauruskuehne/jira-extensions/raw/master/jira-innosolv-ch.user.js
